bcc1266c5c91906315b85fd681c3d57f41a69a23
dongorae <ldjnewdress@gmail.com>
2024-05-22 18:07:06 +0900

    startline

--- config.py
+++ config.py
@@ -14,11 +14,6 @@ starting_line_endpoints = [
     [0, 0]     # point2 : [x2, y2]
 ]
 
-starting_line_endpoints = [
-    [1051, 657],    # point1 : [x1, y1]
-    [1255, 895]     # point2 : [x2, y2]
-]
-
 ######## game rule ########
 total_laps = 2
 offtrack_penalty = 3     # seconds
